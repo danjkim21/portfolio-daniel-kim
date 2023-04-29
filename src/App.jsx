1c91b3cb0c8117c43b2c28b374517fc85f6bee30
danjkim21 <dan.jkim21@gmail.com>
2023-04-29 11:07:30 -0400

    updated router folder to lowercase

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './styles/app.css';
 import './styles/normalize.css';
 
-import ProjectRoutes from './Router/ProjectRoutes';
+import ProjectRoutes from './router/ProjectRoutes';
 
 function App() {
   return (

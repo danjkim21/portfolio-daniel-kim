13aaf59f3f7cbae8d88a222f958ab91e84314758
danjkim21 <dan.jkim21@gmail.com>
2023-04-29 11:06:45 -0400

    moved routes to separate router component and folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,55 +2,13 @@
 import './styles/app.css';
 import './styles/normalize.css';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ScrollToTop from './utils/ScrollToTop';
-
-import projectData from './assets/data/project-details';
-import myData from './assets/data/my-details';
-
-// Components
-import BackgroundImg from './components/background-img';
-import Header from './components/header-section';
-import Hero from './components/hero-section';
-import About from './components/about-section';
-import Projects from './components/projects-section';
-import Footer from './components/footer-section';
-import ProjectDetail from './components/project-detail-section';
+import ProjectRoutes from './Router/ProjectRoutes';
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <ScrollToTop />
-        <BackgroundImg />
-        <Header />
-
-        <Routes>
-          {/* Landing page sections */}
-          <Route
-            path='*'
-            element={
-              <>
-                <Hero {...myData} />
-                <About {...myData} />
-                <Projects projectData={projectData} />
-              </>
-            }
-          />
-          {/* Project Detail page */}
-          <Route
-            path='/project/:projectName'
-            element={
-              <>
-                <ProjectDetail projectData={projectData} />
-              </>
-            }
-          />
-        </Routes>
-
-        <Footer {...myData} />
-      </div>
-    </Router>
+    <div className='App'>
+      <ProjectRoutes />
+    </div>
   );
 }
 
---
+++ src/Router/ProjectRoutes.jsx
@@ -0,0 +1,49 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ScrollToTop from '../utils/ScrollToTop';
+
+import projectData from '../assets/data/project-details';
+import myData from '../assets/data/my-details';
+
+// Components
+import BackgroundImg from '../components/background-img';
+import Header from '../components/header-section';
+import Hero from '../components/hero-section';
+import About from '../components/about-section';
+import Projects from '../components/projects-section';
+import Footer from '../components/footer-section';
+import ProjectDetail from '../components/project-detail-section';
+
+export default function ProjectRoutes() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <BackgroundImg />
+      <Header />
+
+      <Routes>
+        {/* Landing page sections */}
+        <Route
+          path='*'
+          element={
+            <>
+              <Hero {...myData} />
+              <About {...myData} />
+              <Projects projectData={projectData} />
+            </>
+          }
+        />
+        {/* Project Detail page */}
+        <Route
+          path='/project/:projectName'
+          element={
+            <>
+              <ProjectDetail projectData={projectData} />
+            </>
+          }
+        />
+      </Routes>
+
+      <Footer {...myData} />
+    </Router>
+  );
+}

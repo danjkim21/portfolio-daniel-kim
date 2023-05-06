2ff0d593f6a5d294cbe5322343716473090d0cc0
danjkim21 <dan.jkim21@gmail.com>
2023-05-06 11:04:22 -0400

    implmented scroll lock when mobile nav is open

--- src/components/header-section/index.jsx
+++ src/components/header-section/index.jsx
@@ -1,47 +1,71 @@
-import { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import backgroundImg from '../../assets/images/gradient-background.svg';
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useScrollLock } from "../../hooks/useScrollLock";
 
-function Header(props) {
+function Header() {
   const { pathname } = useLocation();
   const [isActive, setIsActive] = useState(false);
+  const { lockScroll, unlockScroll } = useScrollLock();
 
-  let toggleNavMobile = () => {
-    setIsActive((isActiveDefault) => !isActive);
+  const onToggleNavMobile = () => {
+    if (isActive) {
+      unlockScroll();
+    }
+    if (!isActive) {
+      lockScroll();
+    }
+    setIsActive((isActiveDefault) => !isActiveDefault);
+  };
+
+  const onNavigate = () => {
+    unlockScroll();
+    setIsActive(false);
   };
 
   return (
     <header
-      id='header'
-      className={isActive ? 'section__header active' : 'section__header'}
+      id="header"
+      className={isActive ? "section__header active" : "section__header"}
     >
-      <Link className='container__logo' to='/'>
-        <i className='header__logo Icon ph-code'></i>
-        <span className='header__logoName hover-underline-animation'>
+      <Link className="container__logo" to="/">
+        <i className="header__logo Icon ph-code"></i>
+        <span className="header__logoName hover-underline-animation">
           daniel jay-young kim
         </span>
       </Link>
 
-      {pathname === '/' && (
+      {pathname === "/" && (
         <>
           <i
-            className='hamburgerToggleBtn ph-list'
-            onClick={toggleNavMobile}
+            className="hamburgerToggleBtn ph-list"
+            onClick={onToggleNavMobile}
           ></i>
-          <nav className={isActive ? 'nav active' : 'nav'}>
-            <ul className='navList'>
-              <li className='navList__items'>
-                <a href='#about' className='hover-underline-animation'>
+          <nav className={isActive ? "nav active" : "nav"}>
+            <ul className="navList">
+              <li className="navList__items">
+                <a
+                  href="#about"
+                  className="hover-underline-animation"
+                  onClick={onNavigate}
+                >
                   about
                 </a>
               </li>
-              <li className='navList__items'>
-                <a href='#projects' className='hover-underline-animation'>
+              <li className="navList__items">
+                <a
+                  href="#projects"
+                  className="hover-underline-animation"
+                  onClick={onNavigate}
+                >
                   projects
                 </a>
               </li>
-              <li className='navList__items'>
-                <a href='#footer' className='hover-underline-animation'>
+              <li className="navList__items">
+                <a
+                  href="#footer"
+                  className="hover-underline-animation"
+                  onClick={onNavigate}
+                >
                   contact
                 </a>
               </li>
